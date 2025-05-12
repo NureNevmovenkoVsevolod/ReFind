50ffbf5bc2a075f58abbe88066b9b652be20d04d
NureKhudiienkoMykyta <mykyta.khudiienko@nure.ua>
2025-05-12 18:50:03 +0300

    Main page

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,7 +1,39 @@
 import React from "react";
+import AuthNavBar from "../components/AuthNavBar/AuthNavBar";
+import NavBar from "../components/NavBar/NavBar";
+import AuthHeroSection from "../components/AuthHeroSection/AuthHeroSection";
+import HeroSection from "../components/HeroSection/HeroSection";
+import CurrentFindsSection from "../components/CurrentFindsSection/CurrentFindsSection";
+import CurrentLossesSection from "../components/CurrentLossesSection/CurrentLossesSection";
+import AuthFooter from "../components/AuthFooter/AuthFooter";
+import Footer from "../components/Footer/Footer";
 
 function MainPage(props) {
-  return <div></div>;
+  const isLogin = props.isLogin;
+  return (
+    <div>
+      {isLogin ? (
+        <>
+          <AuthNavBar></AuthNavBar>
+          <AuthHeroSection></AuthHeroSection>
+        </>
+      ) : (
+        <>
+          <NavBar></NavBar>
+          <HeroSection></HeroSection>
+        </>
+      )}
+      <CurrentFindsSection />
+      <CurrentLossesSection />
+      {isLogin ? (
+        <AuthFooter></AuthFooter>
+      ) : (
+        <>
+          <Footer></Footer>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default MainPage;
