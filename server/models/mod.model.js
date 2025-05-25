e6afb7caa72e8efcb38a2dc3a06854569f5e06ed
NureKhudiienkoMykyta <mykyta.khudiienko@nure.ua>
2025-05-25 17:23:01 +0300

    Змінив модель модератора

--- server/models/mod.model.js
+++ server/models/mod.model.js
@@ -23,7 +23,26 @@ const Mod = sequelize.define(
   {
     tableName: "mod",
     timestamps: false,
+    hooks: {
+      beforeCreate: async (mod) => {
+        if (mod.password) {
+          mod.password = await bcrypt.hash(mod.password, 5);
+        }
+      },
+      beforeUpdate: async (mod) => {
+        if (mod.changed("password")) {
+          mod.password = await bcrypt.hash(mod.password, 5);
+        }
+        if (mod.changed("is_blocked") && mod.is_blocked) {
+          mod.blocked_at = new Date();
+        }
+      },
+    },
   }
 );
+
+Mod.prototype.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 export default Mod;
