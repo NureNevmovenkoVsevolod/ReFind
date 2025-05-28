ff49a1e028c01b51265a23423ae838b48504c000
NureTarasovRostyslav <rostyslav.tarasov@nure.ua>
2025-05-28 23:43:11 +0300

    Facebook auth fix

--- server/config/passport.js
+++ server/config/passport.js
@@ -82,6 +82,14 @@ passport.use(
         });
 
         if (!user) {
+          await user.update({
+            auth_provider: "google",
+            provider_id: profile.id,
+            user_pfp: profile.photos && profile.photos[0] ? profile.photos[0].value : null,
+          });
+
+          await user.reload();
+        }else{
           user = await User.create({
             email: profile.emails && profile.emails[0] ? profile.emails[0].value : "",
             first_name: profile.name.givenName || "",
